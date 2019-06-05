617144b1997776d20667e4f66eed57c749ecdcba
GGuBooNT <dlgusgh7870@naver.com>
2019-06-06 02:40:02 +0900

    AppDrawer의 app들의 글자 색이 잘 보이지않아 Black 컬러로 변경

--- app/src/main/java/com/benny/openlauncher/widget/AppDrawerGrid.java
+++ app/src/main/java/com/benny/openlauncher/widget/AppDrawerGrid.java
@@ -96,7 +96,7 @@ public class AppDrawerGrid extends FrameLayout {
             App app = apps.get(i);
             items.add(new IconLabelItem(app.getIcon(), app.getLabel())
                     .withIconSize(getContext(), Setup.appSettings().getIconSize())
-                    .withTextColor(Color.BLACK)
+                    .withTextColor(Color.WHITE)
                     .withTextVisibility(Setup.appSettings().getDrawerShowLabel())
                     .withIconPadding(getContext(), 8)
                     .withTextGravity(Gravity.CENTER)
